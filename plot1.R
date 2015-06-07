723d5fc78734bd547bf9a57484ff95b94129285f
rajeshsankar <rajesh.sankar@gmail.com>
2015-06-07 12:34:10 -0500

    Changed data read

--- plot1.R
+++ plot1.R
@@ -4,19 +4,17 @@ pwr <- read.table("data/household_power_consumption.txt", header = TRUE, sep = "
                   colClasses = c("character", "character", "numeric", "numeric", 
                                  "numeric", "numeric", "numeric", "numeric", 
                                  "numeric"),
-                  na.strings = c("?"),
+                  na = "?",
                   fill = TRUE, strip.white = TRUE)
 
-
-
 pwrsub <- subset(pwr, pwr$Date %in% c("1/2/2007", "2/2/2007"))
-as.Date(pwrsub$Date, format="%d/%m/%Y")
+pwrsub$Date <- as.Date(pwrsub$Date, format="%d/%m/%Y")
+pwrsub$Time <- strptime(paste(pwrsub$Date, pwrsub$Time), format="%Y-%m-%d %H:%M:%S")
 
 png("plot1.png", w = 480, h = 480)
 hist(pwrsub$Global_active_power, 
      xlab = "Global Active Power (kilowatts)", 
      main = "Global Active Power",
-     #h = 480,
      col="red")
 
 dev.off()
